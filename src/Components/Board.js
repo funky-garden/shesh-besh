bcdb41a0c7ea25a51eef69d3ac82e3bb73c92df7
Benny <bgalun@gmail.com>
2022-10-04 13:39:26 +0300

    Added alternating colors for arrows

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import './Board.scss'
 
+const arrowColor = (section, index) => {
+    if(section === 'top') {
+        return index % 2 ? 'black' : 'white'
+    }
+    else {
+        return index % 2 ? 'white' : 'black'
+    }
+}
+
 const Arrows = ({section}) => {
-    const name = `arrow-${section}`
-    const arrow = () => {
+    const arrow = (name) => {
         return <div className={name}/>
     }
 
     let arrows = []
     for(let i = 0; i < 6; i++) {
-        arrows.push(arrow())
+        const name = `arrow-${arrowColor(section, i)} arrow-${section}`
+        arrows.push(arrow(name))
     }
     return arrows
 }
